bbe6cc2e2c84d38a3eebac3821d0d65080653253
alebastr12 <thegrace12@gmail.com>
2019-09-15 19:12:37 +0300

    Задание готово! Есть детали и ссылка для возврата к списку.

---
+++ WebStore/lesson1/Models/EmployeeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lesson1.Models
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public string Post { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
--- WebStore/lesson1/Startup.cs
+++ WebStore/lesson1/Startup.cs
@@ -15,6 +15,7 @@ namespace lesson1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,10 @@ namespace lesson1
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            app.UseMvc(routes => routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}"));
 
             app.Run(async (context) =>
             {
