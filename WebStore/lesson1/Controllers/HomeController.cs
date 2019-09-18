6a35e87ad7079261486d4603ff7fb39ef1ab448b
alebastr12 <thegrace12@gmail.com>
2019-09-18 19:12:06 +0300

    перенес макет во View, но пока не правил ссылки.

--- WebStore/lesson1/Controllers/HomeController.cs
+++ WebStore/lesson1/Controllers/HomeController.cs
@@ -12,5 +12,37 @@ namespace lesson1.Controllers
         {
             return View();
         }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
     }
 }
