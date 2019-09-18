1522de8cb1050ceac9c813f673e14b73106f9915
alebastr12 <thegrace12@gmail.com>
2019-09-18 18:33:10 +0300

    Index и _Layout

--- WebStore/lesson1/Controllers/HomeController.cs
+++ WebStore/lesson1/Controllers/HomeController.cs
@@ -2,49 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using lesson1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lesson1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeView> _employeeViews = new List<EmployeeView> {
-            new EmployeeView {
-                Id=1,
-                FirstName="Иван",
-                SurName="Петров",
-                Patronymic="Вячеславович",
-                DateOfBirth=DateTime.Parse("10.12.1956"),
-                Post="Директор"
-            },
-            new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Александр",
-                SurName = "Иванов",
-                Patronymic = "Петрович",
-                DateOfBirth = DateTime.Parse("11.02.1959"),
-                Post = "Уборщик"
-            },
-            new EmployeeView
-            {
-                Id = 3,
-                FirstName = "Виктория",
-                SurName = "Сидоркина",
-                Patronymic = "Альбертовна",
-                DateOfBirth = DateTime.Parse("08.03.1989"),
-                Post = "Секретарша"
-            }
-        };
         public IActionResult Index()
         {
-            //return Content("Привет изконтроллера!");
-            return View(_employeeViews);
-        }
-        public IActionResult Details(int? id)
-        {
-            return View(_employeeViews.FirstOrDefault(e => e.Id == id));
+            return View();
         }
     }
 }
