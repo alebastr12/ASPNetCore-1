fbaff145fb35ef2181d584f02045b349408a4459
alebastr12 <thegrace12@gmail.com>
2019-09-29 19:30:05 +0300

    доделал Catalog/Shop

--- WebStore/lesson1/Controllers/CatalogController.cs
+++ WebStore/lesson1/Controllers/CatalogController.cs
@@ -2,19 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lesson1.Infrastructure.Interfaces;
+using lesson1.Models;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Filters;
 
 namespace lesson1.Controllers
 {
     public class CatalogController : Controller
     {
+        private readonly IProductService _productService;
+
+        public CatalogController(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IActionResult ProductDetails()
         {
             return View();
         }
-        public IActionResult Shop()
+        public IActionResult Shop(int? CategoryId, int? BrandId)
         {
-            return View();
+            var products = _productService.GetProducts(
+                new ProductFilter { BrandId = BrandId, CategoryId = CategoryId });
+
+            // сконвертируем в CatalogViewModel
+            var model = new CatalogViewModel()
+            {
+                BrandId = BrandId,
+                CategoryId = CategoryId,
+                Products = products.Select(p => new ProductViewModel()
+                {
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+
+            return View(model);
         }
     }
 }
